039e75778dfd3c6a81b166b6fb00e44ed2ca0a0c
fireattack <human.peng@gmail.com>
2019-07-15 02:28:36 -0500

    Update for TI9

--- ti_age.py
+++ ti_age.py
@@ -1,12 +1,20 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import time
 
 def getPageFromAPI(pageName):
+
+    # See: https://liquipedia.net/api-terms-of-use
+    time.sleep(2)
+    headers = {'User-Agent': 'ti_age project', 'Accept-Encoding': 'gzip'}
+
     if pageName.startswith('/dota2/'):
         pageName = pageName[7:]
+    pageName = pageName.replace('&amp;', '%26')
     url = f'https://liquipedia.net/dota2/api.php?action=query&prop=revisions&titles={pageName}&rvprop=content&rvparse&format=json'
-    r = requests.get(url)
+    
+    r = requests.get(url, headers=headers)
     json = r.json()
     html = list(json['query']['pages'].values())[0]["revisions"][0]['*']
     if 'redirectMsg' in html:
@@ -15,15 +23,16 @@ def getPageFromAPI(pageName):
     else:
         return BeautifulSoup(html, 'lxml')
 
-divs = getPageFromAPI('The_International/2018').find_all('div', class_='teamcard')
+divs = getPageFromAPI('The_International/2019').find_all('div', class_='teamcard')
 
 for div in divs:
     center = div.find('center')
     teamname = re.search(r'title="(.+?)"', str(center))[1]
+        
     print(teamname)
     for tr in div.find_all('tr'):
         string = re.search(
-            r'<th>([12345])</th><td><a.+</a>.+<a.+href="(.+)".+?title="(.+?)"', str(tr))
+            r'<th>([12345])</th><td>.+<a.+</a>.+<a.+href="(.+)".+?title="(.+?)"', str(tr))
         if string is not None:
             pos = string[1]
             name = string[3]
@@ -34,5 +43,8 @@ for div in divs:
                 bday = 'N/A'
             record = teamname + ',' + pos + ',' + name + ',' + bday
             print(record)
-            with open("output_2018.csv", "a", encoding='utf-8-sig') as f:
+            with open("output_2019.csv", "a", encoding='utf-8-sig') as f:
                 f.write(record + '\n')
+        else:
+            # Not for a player. Ignore.
+            pass
