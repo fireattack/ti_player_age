7d021441675ccb41835c89e378bbdb5df0caf797
fireattack <human.peng@gmail.com>
2023-09-01 16:26:26 +0800

    Update

--- ti_age.py
+++ ti_age.py
@@ -1,19 +1,22 @@
 import re
 import requests
 from bs4 import BeautifulSoup
-import time
 
+
+
+
+YEAR = 2023
 def getPageFromAPI(pageName):
 
     # See: https://liquipedia.net/api-terms-of-use
-    time.sleep(2)
+    # time.sleep(2)
     headers = {'User-Agent': 'ti_age project', 'Accept-Encoding': 'gzip'}
 
     if pageName.startswith('/dota2/'):
         pageName = pageName[7:]
     pageName = pageName.replace('&amp;', '%26')
     url = f'https://liquipedia.net/dota2/api.php?action=query&prop=revisions&titles={pageName}&rvprop=content&rvparse&format=json'
-    
+    print('Getting ' + url)
     r = requests.get(url, headers=headers)
     json = r.json()
     html = list(json['query']['pages'].values())[0]["revisions"][0]['*']
@@ -23,27 +26,28 @@ def getPageFromAPI(pageName):
     else:
         return BeautifulSoup(html, 'lxml')
 
-divs = getPageFromAPI('The_International/2019').find_all('div', class_='teamcard')
+divs = getPageFromAPI(f'The_International/{YEAR}').find_all('div', class_='teamcard')
+
+# print all team names.
+team_names = [div.select_one('center').text for div in divs]
+print(f'Find {len(team_names)} teams: {", ".join(team_names)}')
 
 for div in divs:
-    center = div.find('center')
-    teamname = re.search(r'title="(.+?)"', str(center))[1]
-        
-    print(teamname)
+    team_name = div.select_one('center').text
+    print('Processing ' + team_name)
     for tr in div.find_all('tr'):
-        string = re.search(
-            r'<th>([12345])</th><td>.+<a.+</a>.+<a.+href="(.+)".+?title="(.+?)"', str(tr))
-        if string is not None:
-            pos = string[1]
-            name = string[3]
-            bd = getPageFromAPI(string[2]).find('span', class_='bday')
-            if bd is not None:
-                bday = innerHTML = "".join([str(x) for x in bd.contents])
+        if m := re.search(r'<th>([12345])</th><td>.+<a.+</a>.+<a.+href="(.+)".+?title="(.+?)"', str(tr)):
+            pos = m[1]
+            name = m[3]
+            bd = getPageFromAPI(m[2]).find('div', string='Born:')
+            if bd:
+                bday = bd.parent.find_all('div')[-1].text
             else:
                 bday = 'N/A'
-            record = teamname + ',' + pos + ',' + name + ',' + bday
+
+            record = team_name + ',' + pos + ',' + name + ',' + bday
             print(record)
-            with open("output_2019.csv", "a", encoding='utf-8-sig') as f:
+            with open(f"output_{YEAR}.csv", "a", encoding='utf-8-sig') as f:
                 f.write(record + '\n')
         else:
             # Not for a player. Ignore.
